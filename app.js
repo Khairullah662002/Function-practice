b4c774dfc0f4e70d1477240c930b048bd5c3e282
Khairullah662002 <Khairullah3412@gmail.com>
2025-06-13 22:30:01 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,30 @@
+//1. addTwoNumbers
+function add(num1 , num2){
+    return num1+num2;
+}
+console.log("Addition is  "+add(12,10));
+
+//2. subtractNumbers
+function sub(num1 , num2){
+    return num1-num2;
+}
+console.log("subtraction is  "+sub(12,10));
+
+// 3. multiplyNumbers
+function mul(num1 , num2){
+    return num1*num2;
+}
+console.log("Multiplication is  "+ mul(12,10));
+
+// 4. divideNumbers
+function div(num1 , num2){
+    return num1/num2;
+}
+console.log("Division is  "+div(12,10));
+// 5. getRemainder
+function rem(num1 , num2){
+    return num1%num2;
+}
+console.log("Reminder is  "+rem(12,10));
+
+
